b60f5de11a61f8d171dfac7dfd7765de164081e2
Tim Schwartz <tim@metaverse.systems>
2019-11-22 12:29:51 -0600

    Added cycle_shape, velocity, draw components

---
+++ components/cycle_shape/src/cycle_shape.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <libecs-cpp/ecs.hpp>
+
+class cycle_shape : public ecs::Component
+{
+  public:
+    cycle_shape(); 
+    cycle_shape(Json::Value);
+    Json::Value save();
+    uint32_t width, height;
+    uint8_t r, g, b, a;
+};
