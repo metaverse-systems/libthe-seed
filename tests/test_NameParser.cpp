8ea3f19db058f491442efc53f73426a06abb3faa
Tim Schwartz <tim@metaverse.systems>
2023-11-08 09:28:10 -0600

    test_NameParser

--- tests/test_NameParser.cpp
+++ tests/test_NameParser.cpp
@@ -2,4 +2,34 @@
 #include <catch2/catch.hpp>
 #include "../src/NameParser.hpp"
 
+TEST_CASE("NameParser constructs with valid input", "[NameParser]") {
+    SECTION("Parses organization and library names") {
+        NameParser np("org-name/lib-name");
+        REQUIRE(np.org == "org-name");
+        REQUIRE(np.library == "lib-name");
+    }
 
+    SECTION("Handles only library name") {
+        NameParser np("library-only");
+        REQUIRE(np.org.empty());
+        REQUIRE(np.library == "library-only");
+    }
+}
+
+TEST_CASE("NameParser throws with invalid input", "[NameParser]") {
+    SECTION("Throws when input starts with a slash") {
+        REQUIRE_THROWS_AS(NameParser("/invalid"), std::invalid_argument);
+    }
+
+    SECTION("Throws when input ends with a slash") {
+        REQUIRE_THROWS_AS(NameParser("invalid/"), std::invalid_argument);
+    }
+
+    SECTION("Throws when input contains multiple consecutive slashes") {
+        REQUIRE_THROWS_AS(NameParser("invalid//name"), std::invalid_argument);
+    }
+
+    SECTION("Throws when input contains multiple slashes") {
+        REQUIRE_THROWS_AS(NameParser("org/name/extra"), std::invalid_argument);
+    }
+}
